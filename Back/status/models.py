0645a752f3f7288f029c706699b80ad05ae05a50
LuanMFC <luanmatheus198@gmail.com>
2024-09-27 11:38:33 -0300

    Feat: modifications to the model

--- Back/status/models.py
+++ Back/status/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from kanban.models import KanBan
 
 # Create your models here.
 class Status(models.Model):
@@ -6,7 +7,7 @@ class Status(models.Model):
   description = models.TextField()
   color = models.CharField(max_length=10)
   date_created = models.DateTimeField()
-
+  kanban = models.ForeignKey(KanBan, on_delete=models.PROTECT, related_name='kanban_status')
 
   def __str__(self):
     return self.name

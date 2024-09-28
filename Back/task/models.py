4a836dac0419e5cc80b5c0fd16800ae73a538f42
Luan_MFC <luanmatheus198@gmail.com>
2024-09-27 23:11:42 -0300

    Feat: Add functions History in Task

--- Back/task/models.py
+++ Back/task/models.py
@@ -3,17 +3,18 @@ from status.models import Status
 from priority.models import Priority
 from django.contrib.auth.models import User
 from kanban.models import KanBan
+from simple_history.models import HistoricalRecords
 
 class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     priority = models.ForeignKey(
-        Status,
+        Priority,
         on_delete=models.PROTECT,
         related_name='task_status'
     )
     status = models.ForeignKey(
-        Priority,
+        Status,
         on_delete=models.PROTECT,
         related_name='task_priority'
     )
@@ -22,6 +23,8 @@ class Task(models.Model):
     date_created = models.DateTimeField(auto_now=True)
     expected_conclusion = models.DateField(blank=True, null=True)
     date_completed = models.DateTimeField(blank=True, null=True)
+
+    history = HistoricalRecords()  # This will add history records to the model.
 
     def __str__(self):
         return self.title

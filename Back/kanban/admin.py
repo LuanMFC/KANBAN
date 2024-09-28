0be722445c1ee04609f61ceaeb23f91e2a6eccbf
Luan_MFC <luanmatheus198@gmail.com>
2024-09-27 23:13:38 -0300

    Feat: Add Kanban admin

--- Back/kanban/admin.py
+++ Back/kanban/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import KanBan
 
 # Register your models here.
+@admin.register(KanBan)
+class KanBanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
